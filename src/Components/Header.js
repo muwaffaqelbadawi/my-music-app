5bc2cd07893f0235ea523a38ff3bd61d1b6ce77f
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-08-10 16:06:09 +0200

    styled the project a liitle bit-2

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../CSS/Header.css";
+
+function Header() {
+  return (
+    <header className='header'>
+        <h1>This is whare the Flex name will go</h1>
+    </header>
+  )
+}
+
+export default Header
